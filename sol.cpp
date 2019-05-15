7049ac95a4cc910ee3812dd9e57e72fd52f0b081
Aykut Sahin <aykut.sahin98@hotmail.com>
2019-05-15 19:08:45 +0200

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -1,1 +1,31 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+bool solveProblem (const int & digit, const string & seq) {
+	if (digit < 11) return false;
+	size_t index = seq.find ('8');
+	if (index == string::npos) return false;
+	if (digit - index < 11) return false;
+	return true;	
+}
+
+int main (void) {
+
+	ios_base::sync_with_stdio (false);
+	cin.tie (NULL);
+	
+	int numTest;
+	int digit;
+	string sequence;
+
+	cin >> numTest;
+	for (int i = 0; i < numTest; i++) {
+		cin >> digit >> sequence;
+		if (solveProblem (digit, sequence) ) cout << "YES" << '\n';
+		else cout << "NO" << '\n';
+		
+	}
+  
+	return 0;
+}
